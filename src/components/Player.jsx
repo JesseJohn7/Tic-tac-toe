1d64ddf2c47d4fffb9e10979f91a74ff6ea99af9
JesseJohn7 <jessejohn260@gmail.com>
2025-06-07 00:31:25 +0100

    initial commit

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,11 +3,13 @@ import { useState } from 'react'
 const Player = ({name,symbol}) => {
   const [isEditing , SetIsEditing] = useState(false)
   function handleEditClick (){
-    SetIsEditing(true)
+    SetIsEditing(!isEditing)
   }
   let playerName =  <span className='player-name'>{name}</span>
+  let buttonCaption = 'Edit'
   if (isEditing){
-    playerName = <input type='text' required/>
+    playerName = <input type='text' required value={name}/>
+    buttonCaption = 'Save'
   }
   return (
     <>
@@ -16,7 +18,7 @@ const Player = ({name,symbol}) => {
             {playerName}
             <span className='player-symbol'>{symbol}</span>
           </span>
-          <button onClick={handleEditClick}>Edit</button>
+            <button onClick={handleEditClick}>{buttonCaption}</button>
           </li>
     </>
   )
